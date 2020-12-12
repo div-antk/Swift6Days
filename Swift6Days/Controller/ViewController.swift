8d4d64ad2df63f980d0dbece57f53b932db88a4e
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-12 11:15:59 +0900

    セルの構築

--- Swift6Days/Controller/ViewController.swift
+++ Swift6Days/Controller/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  @IBOutlet weak var tableView: UITableView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    tableView.delegate = self
+    tableView.dataSource = self
   }
-
-
+  
+  // セルの数
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  // セルの構築
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    
+    let userNameLabel = cell.contentView.viewWithTag(1) as! UILabel
+    
+    let hobbyLabel = cell.contentView.viewWithTag(2) as! UILabel
+    
+    let moviewLabel = cell.contentView.viewWithTag(3) as! UILabel
+    
+    return cell
+  }
+  
+  
 }
 
