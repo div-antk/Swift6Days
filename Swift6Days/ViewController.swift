f910662b6135842bc923d8075d73d7c868d0e5cf
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-12 10:37:28 +0900

    Initial Commit

---
+++ Swift6Days/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6Days
+//
+//  Created by Takuya Ando on 2020/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
